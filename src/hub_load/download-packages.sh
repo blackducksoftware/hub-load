b8fedd049d9f4c84d9883e05da8b157e7f5495ff
Glenn Snyder <gsnyder@blackducksoftware.com>
2019-10-02 19:33:06 -0400

    changing way we populate the jars folder

--- src/hub_load/download-packages.sh
+++ src/hub_load/download-packages.sh
@@ -5,8 +5,9 @@
 WORKDIR=$(dirname $0)
 mkdir -p $WORKDIR/jars
 
-for i in `cat $WORKDIR/packagelist`
-do
-  echo Expanding $i int $WORKDIR/jars
-  curl -s $i | tar zxf -  -C $WORKDIR/jars
-done
+echo "Downloading and unpacking jars.zip from S3"
+wget https://bds-sa-data-files.s3.us-east-2.amazonaws.com/jars.zip
+unzip jars.zip
+echo "Removing jars.zip now that we have unzipped it"
+rm jars.zip
+
