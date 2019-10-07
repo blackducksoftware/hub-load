8832ed0984f18e03f9c429e0ef49f15aa86244e2
Glenn Snyder <gsnyder@blackducksoftware.com>
2019-10-07 11:03:47 -0400

    refactoring submit_scans.sh to include more parameters for detect, make the loop contingent on the number of scans; refactoring download script to optionally use a locally populated jars directory for building the docker image from

--- src/hub_load/submit_scans.sh
+++ src/hub_load/submit_scans.sh
@@ -38,9 +38,12 @@ cd $WORKDIR
 #
 # Defaults
 #
+BD_TIMEOUT=${BD_TIMEOUT:-120}
+API_TIMEOUT=${API_TIMEOUT:-300000}
 MAX_SCANS=${MAX_SCANS:-10}
 MAX_CODELOCATIONS=${MAX_CODELOCATIONS:-1}
-MAX_COMPONENTS=${MAX_COMPONENTS:-150}
+MAX_COMPONENTS=${MAX_COMPONENTS:-400}
+MIN_COMPONENTS=${MIN_COMPONENTS:-200}
 MAX_VERSIONS=${MAX_VERSIONS:-5}
 SYNCHRONOUS_SCANS=${SYNCHRONOUS_SCANS:-yes}
 REPEAT_SCAN=${REPEAT_SCAN:-no}
@@ -49,7 +52,7 @@ REPEAT_SCAN=${REPEAT_SCAN:-no}
 PROJECT="Project-$HOSTNAME"
 TIMESTAMP=$(date +%Y%m%d.%H%M%S)
 
-INT_PARAMS="BD_HUB_URL API_TOKEN MAX_SCANS MAX_CODELOCATIONS MAX_COMPONENTS MAX_VERSIONS REPEAT_SCAN SYNCHRONOUS_SCANS"
+INT_PARAMS="BD_HUB_URL API_TOKEN BD_TIMEOUT API_TIMEOUT MAX_SCANS MAX_CODELOCATIONS MIN_COMPONENTS MAX_COMPONENTS MAX_VERSIONS REPEAT_SCAN SYNCHRONOUS_SCANS"
 
 if [ "$INTERACTIVE" = "yes" ]
 then
@@ -89,6 +92,11 @@ then
    exit 1
 fi
 
+if (( $MAX_COMPONENTS <= $MIN_COMPONENTS )); then
+  echo "MAX_COMPONENTS must be greater than MIN_COMPONENTS"
+  exit 1
+fi
+
 #
 #  Generate an array of available JAR files
 #
@@ -109,29 +117,40 @@ echo "starting"
 pos=0
 scans=0
 repeating=no
-while [ $pos -lt ${#jars[@]} ]
+# while [ $pos -lt ${#jars[@]} ]
+
+while (( scans < MAX_SCANS ))
 do
   echo "do"
   if [ "${repeating}" == "no" ]; then
+    start_pos=$(( ( RANDOM % ${#jars[@]} ) ))
     num_jars=$(( ( RANDOM % $MAX_COMPONENTS ) + 1 ))
-    end=$((pos + num_jars))
+    # use maximum of num_jars OR MIN_COMPONENTS to set lower threshold for number of components
+    num_jars=$(( num_jars > MIN_COMPONENTS ? num_jars : MIN_COMPONENTS ))
+    end=$((start_pos + num_jars))
     if [ $end -gt ${#jars[@]} ]
     then
-      num_jars=$((#jars[@] - pos))
+      num_jars=$((#jars[@] - start_pos))
     fi
     project_jars=("${jars[@]:$pos:$num_jars}")
+    echo "start_pos: $start_pos"
+    echo "num_jars: $num_jars"
+    echo "end: $end"
+    echo "jars in project_jars: ${#project_jars[@]}"
+    echo "project_jars: ${project_jars[@]}"
   fi
 
   repeating=${REPEAT_SCAN}
 
   project_name="$PROJECT-$(($RANDOM))-on-${TIMESTAMP}"
+  echo "project_name: ${project_name}"
   mkdir $project_name
 
   RANDOM=`date "+%s"`
   versions=$(( ( RANDOM % $MAX_VERSIONS ) + 1 ))
   for ((v=1; v<=$versions;v++))
   do
-    echo "do again"
+    echo "version: $v"
     num_codelocations=$(( ( RANDOM % 10 ) + 1 ))
     # We don't want to overload code-locations unnecessafily.
     # Theres enough slowness as is.  MAX_CODELOCATIONS is a good handbrake
@@ -141,9 +160,10 @@ do
     fi
     for ((cl=0; cl<$num_codelocations;cl++))
     do
-      echo "here we goooo"
+      echo "code location: $(( cl + 1 ))"
       RANDOM=`date "+%s"`
-      cl_name="codelocation-$((RANDOM))"
+      cl_name="${project_name}-v${v}-cl-$((RANDOM))"
+      echo "code location name: $cl_name"
       # echo "1"
       # set +e
 
@@ -156,6 +176,8 @@ do
       DETECT_OPTIONS="${DETECT_OPTIONS} --detect.project.name=${project_name} --detect.project.version.name=${v}"
       DETECT_OPTIONS="${DETECT_OPTIONS} --detect.code.location.name=${cl_name}"
       DETECT_OPTIONS="${DETECT_OPTIONS} --blackduck.trust.cert=true"
+      DETECT_OPTIONS="${DETECT_OPTIONS} --detect.api.timeout=${API_TIMEOUT}"
+      DETECT_OPTIONS="${DETECT_OPTIONS} --blackduck.timeout=${BD_TIMEOUT}"
       DETECT_OPTIONS="${DETECT_OPTIONS} --detect.blackduck.signature.scanner.parallel.processors=-1"
       DETECT_OPTIONS="${DETECT_OPTIONS} --detect.tools=SIGNATURE_SCAN"
       DETECT_OPTIONS="${DETECT_OPTIONS} --detect.source.path=${project_name}/${cl_name}"
@@ -169,14 +191,10 @@ do
       rm $detect_log
 
       ((scans++))
-      echo "Total scans submitted: $scans"
-      if [ $scans -ge $MAX_SCANS ]
-      then
-        exit 0
-      fi
     done
     echo "looping"
   done
+  echo "Removing ${project_name}"
   rm -rf $project_name
-  pos=$((pos + num_jars + 1))
+  # pos=$((pos + num_jars + 1))
 done
