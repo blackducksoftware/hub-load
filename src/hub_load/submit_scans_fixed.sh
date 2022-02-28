645d459688475d2f50bdd8b06bcbafa9165f6464
karth <karth@synopsys.com>
2022-02-28 13:28:39 -0700

    Added Traditional Scan Support with Throttling.

--- src/hub_load/submit_scans_fixed.sh
+++ src/hub_load/submit_scans_fixed.sh
@@ -4,11 +4,11 @@
 #
 
 function readvar() {
-   echo -n "Enter value for $1 [${!1}] "
-   read temp
-   if [ ! -z $temp ] ; then 
-     eval $1=$temp
-   fi
+  echo -n "Enter value for $1 [${!1}] "
+  read temp
+  if [ ! -z $temp ]; then
+    eval $1=$temp
+  fi
 }
 
 function get_elapsed_time() {
@@ -21,13 +21,13 @@ function get_elapsed_time() {
   seconds=$(echo $duration_line | awk '{print $11}' | sed -e "s/s//")
   # echo "Seconds: $seconds"  1>&2
 
-  total_elapsed_seconds=$(( $hours * 3600 + $minutes * 60 + $seconds))
+  total_elapsed_seconds=$(($hours * 3600 + $minutes * 60 + $seconds))
   # echo "total elapsed time (seconds): $total_elapsed_seconds" 1>&2
 
   # the total elapsed time is written to stdout so you can use this as input to something else
   # that reads from stdout
   #
-  echo $total_elapsed_seconds  
+  echo $total_elapsed_seconds
 }
 
 # Set woring directory
@@ -38,30 +38,39 @@ cd $WORKDIR
 #
 # Defaults
 #
-BD_HUB_URL=${BD_HUB_URL:-https://}
-API_TOKEN=${API_TOKEN:-Y2Y0N}
+BD_HUB_URL=${BD_HUB_URL:-https://karthik-bd-xxlarge.saas-staging.blackduck.com}
+API_TOKEN=${API_TOKEN:-NTJjZGVkY2EtNTQxZC00ZGUxLTg4MDYtMDU4YTk2ZjVkY2NjOmVlNWJkNjNiLTFiMjAtNGJkYS1hNmI5LWExYmY2NjY4ODgwNw==}
 API_TIMEOUT=${API_TIMEOUT:-300}
 MAX_SCANS=${MAX_SCANS:-3}
 MAX_CODELOCATIONS=${MAX_CODELOCATIONS:-1}
-MAX_COMPONENTS=${MAX_COMPONENTS:-400}
-MIN_COMPONENTS=${MIN_COMPONENTS:-200}
-FIXED_COMPONENTS=${FIXED_COMPONENTS:-100}
+FIXED_COMPONENTS=${FIXED_COMPONENTS:-50}
 MAX_VERSIONS=${MAX_VERSIONS:-1}
-SYNCHRONOUS_SCANS=${SYNCHRONOUS_SCANS:-yes}
+SYNCHRONOUS_SCANS=${SYNCHRONOUS_SCANS:-no}
 REPEAT_SCAN=${REPEAT_SCAN:-no}
 RANDOM_SCANS=${RANDOM_SCANS:-no}
 DETECT_VERSION=${DETECT_VERSION}
 FAIL_ON_SEVERITIES=${FAIL_ON_SEVERITIES}
 INSECURE_CURL=${INSECURE_CURL:-no}
+WAIT_TIME=${WAIT_TIME:-30}
+TEST_DURATION=${TEST_DURATION:-1}
+TARGET_DURATION=0
 
-if [ -z "${DETECT_VERSION}" ]
-then
+
+if [ -z "$TEST_DURATION" ]; then
+  echo "Scans will be submitted as fast it can, continuing."
+  exit 1
+else
+  #target rate / Scan
+  TARGET_DURATION=$(((TEST_DURATION * 3600) / MAX_SCANS))
+  echo "Scans will be submitted at the rate of 1 scan per ${TARGET_DURATION} seconds"
+fi
+
+if [ -z "${DETECT_VERSION}" ]; then
   echo "Default Detect Version"
   DETECT_VERSION="LATEST"
 fi
 
-if [ -z "${FAIL_ON_SEVERITIES}" ]
-then
+if [ -z "${FAIL_ON_SEVERITIES}" ]; then
   echo "Default Fail on severities"
   FAIL_ON_SEVERITIES="NONE"
 fi
@@ -70,87 +79,79 @@ PROJECT="Project-$HOSTNAME"
 TIMESTAMP=$(date +%Y%m%d.%H%M%S)
 
 INT_PARAMS="BD_HUB_URL API_TOKEN API_TIMEOUT MAX_SCANS MAX_CODELOCATIONS MAX_VERSIONS REPEAT_SCAN SYNCHRONOUS_SCANS DETECT_VERSION FAIL_ON_SEVERITIES INSECURE_CURL RANDOM_SCANS FIXED_COMPONENTS"
-
-if [ "$INTERACTIVE" = "yes" ]
-then
-   for i in $INT_PARAMS
-   do
-     readvar $i
-   done
+if [ "$INTERACTIVE" = "yes" ]; then
+  for i in $INT_PARAMS; do
+    readvar $i
+  done
 fi
 
-echo 
+echo
 echo "Submitting with the following parameters:"
-echo  
-for i in $INT_PARAMS
-do
-   echo $'\t' $i ${!i}
+echo
+for i in $INT_PARAMS; do
+  echo $'\t' $i ${!i}
 done
 
-echo 
-if [ "$INTERACTIVE" = "yes" ]
-then
-   continue=Y
-   readvar continue
-   if [[ ! "$continue" = "Y" ]] ; then exit 1 ; fi
+echo
+if [ "$INTERACTIVE" = "yes" ]; then
+  continue=Y
+  readvar continue
+  if [[ ! "$continue" == "Y" ]]; then exit 1; fi
 fi
 
 echo Starting ...
 
-if [ -z "$BD_HUB_URL" ]
-then
-   echo No Black Duck URL specified, Exiting.
-   exit 1
+if [ -z "$BD_HUB_URL" ]; then
+  echo No Black Duck URL specified, Exiting.
+  exit 1
 fi
 
-if [ -z "$API_TOKEN" ]
-then
-   echo No API token specified, Exiting.
-   exit 1
+if [ -z "$API_TOKEN" ]; then
+  echo No API token specified, Exiting.
+  exit 1
 fi
 
-if (( $MAX_COMPONENTS <= $MIN_COMPONENTS )); then
+if [[ "$MAX_COMPONENTS" -lt "$MIN_COMPONENTS" ]]; then
   echo "MAX_COMPONENTS must be greater than MIN_COMPONENTS"
   exit 1
 fi
 
-if [ "${DETECT_VERSION}" != "LATEST" ]
-then
+if [ "${DETECT_VERSION}" != "LATEST" ]; then
   echo "Using Detect Version ${DETECT_VERSION}"
   export DETECT_LATEST_RELEASE_VERSION=${DETECT_VERSION}
-else 
+else
   echo "Using Latest Detect Version"
 fi
 
-if [ "${FAIL_ON_SEVERITIES}" != "NONE" ]
-then
+if [ "${FAIL_ON_SEVERITIES}" != "NONE" ]; then
   echo "Using FAIL_ON_SEVERITIES ${FAIL_ON_SEVERITIES}"
-else 
+else
   echo "Not specifying FAIL_ON_SEVERITIES"
 fi
 
 if [ "${INSECURE_CURL}" == "yes" ]; then
-	echo "Setting environment variable DETECT_CURL_OPTS=--insecure"
-	export DETECT_CURL_OPTS=--insecure
+  echo "Setting environment variable DETECT_CURL_OPTS=--insecure"
+  export DETECT_CURL_OPTS=--insecure
 fi
 
 #
 #  Generate an array of available JAR files
 #
 echo ".............................."
-OIFS=$IFS; IFS=$'\n';
-jars=($(find . -name \*.jar -print | sort -V))
-IFS=$OIFS;
+OIFS=$IFS
+IFS=$'\n'
+
+jars=($(find . -name \*.jar -print))
+IFS=$OIFS
 
 echo ${#jars[@]} jar files located
 echo "...................................."
-
 
 #
 # Seed random number generator
 #
 RANDOM=$(date "+%s")
-echo "starting" 
+echo "starting"
 pos=0
 scans=0
 repeating=no
@@ -159,17 +160,15 @@ start_pos=0
 end=10
 # while [ $pos -lt ${#jars[@]} ]
 
-while (( scans < MAX_SCANS ))
-do
+while ((scans < MAX_SCANS)); do
   echo "do"
   if [ "${repeating}" == "no" ] && [ "${RANDOM_SCANS}" == "yes" ]; then
-    start_pos=$(( ( RANDOM % ${#jars[@]} ) ))
-    num_jars=$(( ( RANDOM % $MAX_COMPONENTS ) + 1 ))
+    start_pos=$(((RANDOM % ${#jars[@]})))
+    num_jars=$(((RANDOM % $MAX_COMPONENTS) + 1))
     # use maximum of num_jars OR MIN_COMPONENTS to set lower threshold for number of components
-    num_jars=$(( num_jars > MIN_COMPONENTS ? num_jars : MIN_COMPONENTS ))
+    num_jars=$((num_jars > MIN_COMPONENTS ? num_jars : MIN_COMPONENTS))
     end=$((start_pos + num_jars))
-    if [ $end -gt ${#jars[@]} ]
-    then
+    if [ $end -gt ${#jars[@]} ]; then
       num_jars=$((${#jars[@]} - start_pos))
     fi
     project_jars=("${jars[@]:$pos:$num_jars}")
@@ -178,23 +177,16 @@ do
     echo "end: $end"
     echo "jars in project_jars: ${#project_jars[@]}"
     echo "project_jars: ${project_jars[@]}"
-  # checking for Random Scans Flag. If the flag is set to No, components chosen to submit scans will be repeatable between releases.
-  elif [  "${RANDOM_SCANS}" == "no"  ]; then
+  elif [ "${RANDOM_SCANS}" == "no" ]; then
     start_pos=$((start_pos + 1))
 
-# assigning the number of components to be submitted per scan based on the total number of jar files available and number of components chosen by the tester.
-    num_jars=$(( FIXED_COMPONENTS > ${#jars[@]} ? ${#jars[@]} : FIXED_COMPONENTS ))
+    num_jars=$((FIXED_COMPONENTS > ${#jars[@]} ? ${#jars[@]} : FIXED_COMPONENTS))
     end=$((start_pos + num_jars))
 
-#Since the jar files files are submited by increasing the value of start and end index, condition is added to check
-# whether the end index value reached the total number of files and if reached resetting it back to 0
-    if [ $end -gt ${#jars[@]} ]
-    #if [ $end -gt 744 ]
-    then
+    if [ $end -gt ${#jars[@]} ]; then #if [ $end -gt 744 ]
       start_pos=0
       end=$num_jars
     fi
-    #start and end index for choosing the jars are assigned.
     project_jars=("${jars[@]:$start_pos:$num_jars}")
     echo "FIXED_COMPONENTS: $FIXED_COMPONENTS"
     echo "start_pos: $start_pos"
@@ -211,18 +203,19 @@ do
   echo "project_name: ${project_name}"
   mkdir $project_name
 
-  RANDOM=`date "+%s"`
-  versions=$(( ( RANDOM % $MAX_VERSIONS ) + 1 ))
-  for ((v=1; v<=$versions;v++))
-  do
+  RANDOM=$(date "+%s")
+  versions=$(((RANDOM % $MAX_VERSIONS) + 1))
+
+  for ((v = 1; v <= $versions; v++)); do
     echo "version: $v"
     num_codelocations=$MAX_CODELOCATIONS
 
-    for ((cl=0; cl<$num_codelocations;cl++))
-    do
-      echo "code location: $(( cl + 1 ))"
-      RANDOM=`date "+%s"`
-      cl_name="${project_name}-v${v}-cl-$((RANDOM))"
+    for ((cl = 0; cl < $num_codelocations; cl++)); do
+      echo "code location: $((cl + 1))"
+      RANDOM=$(date "+%s")
+      container_id=`cat /etc/hostname`
+      echo "Container ID: $container_id"
+      cl_name="$container_id-cl-${cl_pos}"
       echo "code location name: $cl_name"
       # echo "1"
       # set +e
@@ -251,6 +244,7 @@ do
       bash <(curl -s -L ${DETECT_CURL_OPTS} https://detect.synopsys.com/detect.sh) ${DETECT_OPTIONS} | tee ${detect_log}
       elapsed_time=$(get_elapsed_time $detect_log)
       echo "Elapsed time for scan was ${elapsed_time} seconds"
+      WAIT_TIME=$(( TARGET_DURATION  - elapsed_time ))
       rm $detect_log
       ((scans++))
     done
@@ -258,5 +252,7 @@ do
   done
   echo "Removing ${project_name}"
   rm -rf $project_name
+  echo "Sleeping for ${WAIT_TIME} seconds based on the ${TARGET_DURATION} seconds per scan"
+  sleep "$WAIT_TIME"
   # pos=$((pos + num_jars + 1))
 done
