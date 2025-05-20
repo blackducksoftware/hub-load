55d67b45216d8de964c0b8768530066f2f258f50
sig-karth <81329501+sig-karth@users.noreply.github.com>
2025-05-20 14:00:21 -0600

    Update submit_scans_fixed.sh

--- src/hub_load/submit_scans_fixed.sh
+++ src/hub_load/submit_scans_fixed.sh
@@ -288,7 +288,7 @@ do
 
       detect_log=/tmp/detect_$$.log
       echo "$(date '+%Y-%m-%d %H:%M:%S') - Final Detect Options: $DETECT_OPTIONS"
-      bash <(curl -s -L ${DETECT_CURL_OPTS} https://detect.synopsys.com/detect9.sh) ${DETECT_OPTIONS} | tee ${detect_log}
+      bash <(curl -s -L ${DETECT_CURL_OPTS} https://detect.blackduck.com/detect10.sh) ${DETECT_OPTIONS} | tee ${detect_log}
       elapsed_time=$(get_elapsed_time $detect_log)
 
       echo "$(date '+%Y-%m-%d %H:%M:%S') - Elapsed time for scan was ${elapsed_time} seconds"
